0e2dcc2df10b85a4504c030cbce112f2f0f1fda4
Farsana_ibrahim <farsanaibrahim987@email.com>
2026-05-19 17:22:03 +0530

    resolved errors

--- routers/auth.py
+++ routers/auth.py
@@ -4,7 +4,7 @@ POST /api/admin/login  → validate password, return admin token
 """
 import os
 from datetime import datetime, timedelta
-import jwt
+from jose import jwt
 from passlib.context import CryptContext
 
 from dotenv import load_dotenv
